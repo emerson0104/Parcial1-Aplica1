192500b7cc7e3a0b58bd6aebe09e7bc02c6a06c8
Emerson Del Orbe Candelario <emerson_delorbe@ucne.edu.do>
2019-09-25 14:40:24 -0400

    creada la DAL

---
+++ PrimerParcial/PrimerParcial/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial.DAL
+{
+   public class Contexto:DbContext
+    {
+      
+            public Contexto() : base("conStr")
+            {
+
+            }
+        }
+    }
+
